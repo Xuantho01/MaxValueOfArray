2ab7938398950493a65a8f6e1e79bde5aba14d6a
Xuantho01 <hoxuantho95@gmail.com>
2020-02-16 11:14:35 +0700

    update file

--- src/MaxValueOfArray.java
+++ src/MaxValueOfArray.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 public class MaxValueOfArray {
     public static void main(String[] args) {
-        int total = 0;
         int[][] array = {{1,2,3},{4,5,6},{7,8,9}};
+        int max = array[0][0];
         for(int i = 0; i < array.length; i++){
             for(int j = 0 ; j < array[i].length; j++){
-                total += array[i][j];
+                if(max < array[i][j]){
+                    max = array[i][j];
+                }
             }
         }
-        System.out.println("total of array: "+total);
+        System.out.println("Max value in array: "+max);
     }
 }
